9982a83a5b5809a713920df834a24f1e8ebc7bcb
hu <1048448520@qq.com>
2021-05-24 19:46:42 +0800

    init

---
+++ run_batch.py
@@ -0,0 +1,58 @@
+import os
+import subprocess
+import shutil
+
+# for each .tgz, unzip it to /tgz_folder, do recusive thing to the /tgz_folder, 
+# write res to the fold /res, and then delete the /tgz_folder
+
+def run_ostrich(newDir):
+    global cmd 
+    ret = subprocess.run(f"{cmd} {newDir}",shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,encoding="utf-8")
+    print(f"{newDir}:\n {ret.stdout}")
+    with open("res.txt", "a", encoding="utf8") as f:
+        f.write(f"{newDir}:\n {ret.stdout}\n")
+
+
+def handle_tar_rec(folderpath):
+    global fileTailNum
+    global resFolderName
+    pathDir = os.listdir(folderpath)      #获取当前路径下的文件名，返回List
+    for s in pathDir:
+        newDir=os.path.join(folderpath,s)     #将文件命加入到当前文件路径后面
+        if os.path.isfile(newDir) :         #如果是文件
+            if os.path.splitext(newDir)[1]==".smt2":
+                run_ostrich(newDir)
+        else:
+            handle_tar_rec(newDir)                #如果不是文件，递归这个文件夹的路径
+
+def handle_tar(tgzpath):
+    tmp_package = os.path.splitext(tgzpath)[0]
+    if os.path.exists(tmp_package):
+        shutil.rmtree(tmp_package)
+    os.mkdir(tmp_package)
+    os.system('echo tar -xzvf ' + tgzpath + ' -C ' + tmp_package)
+    os.system('tar -xzvf ' + tgzpath + ' -C ' + tmp_package + " >tmp.txt 2>&1") 
+    handle_tar_rec(tmp_package)
+    shutil.rmtree(tmp_package)
+
+
+
+def eachFile(filepath):
+    pathDir = os.listdir(filepath)      #获取当前路径下的文件名，返回List
+    for s in pathDir:
+        newDir=os.path.join(filepath,s)     #将文件命加入到当前文件路径后面
+        if os.path.isfile(newDir) :         #如果是文件
+            if os.path.splitext(newDir)[1]==".tar":  #判断是否是tar
+                handle_tar(newDir)                     #读文件（或者做其他的操作）
+                pass
+        else:
+            eachFile(newDir)                #如果不是文件，递归这个文件夹的路径
+
+fileTailNum = 0
+cmd = "python3 script.py"
+
+# clear the existing res file
+with open("res.txt", "r") as f:
+    f.close()
+
+eachFile('.')
---
+++ script.py
@@ -0,0 +1,53 @@
+import os
+import psutil
+import signal
+import subprocess
+import sys
+from multiprocessing import Process, Manager
+
+
+def execmd(cmd, args, v, pid) :
+    r = subprocess.Popen(cmd+args, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, preexec_fn=os.setsid, encoding="utf8")
+    pid.append(r.pid)
+    info = r.stdout.readlines()
+    v.append(info[-2:])     # the last 2 lines: e.g ["sat\n", "1.11s\n"]
+
+def cpExecutableJar():
+    if(not os.path.exists("./ostrich-assembly-1.0.jar")):
+        os.system("cp ./target/scala-*/ostrich-assembly*.jar ostrich-assembly-1.0.jar")
+
+if __name__== "__main__":
+    if len(sys.argv) != 2:
+        print("use : python script.py fileName")
+        exit(1)
+    cpExecutableJar()
+    manager = Manager()
+    ic3 = manager.list()
+    bmc = manager.list()
+    ic3_pid = manager.list()
+    bmc_pid = manager.list()
+    cmd1 = "java -jar ostrich-assembly-1.0.jar -timeout=30 -window=0 -strategy=-I -tmpfile=tmp1.txt "
+    cmd2 = "java -jar ostrich-assembly-1.0.jar -timeout=30 -window=30 -strategy=-F -tmpfile=tmp2.txt "
+    p_ic3 = Process(target = execmd, args = (cmd1, sys.argv[1] + " 2>&1", ic3, ic3_pid))
+    p_ic3.daemon = True
+    p_bmc = Process(target = execmd, args = (cmd2, sys.argv[1] + " 2>&1", bmc, bmc_pid))
+    p_bmc.daemon = True
+    p_ic3.start()
+    p_bmc.start()
+    while (len(ic3) == 0) & (len(bmc) == 0) :
+        pass
+    if len(ic3) > 0 :
+        os.killpg(bmc_pid[0], signal.SIGKILL)
+        print(ic3[0][0][0:-1])  # 0:-1 is used to delete "\n"
+        #p_ic3.join()
+    elif 'sat\n' in bmc[0] :
+        os.killpg(ic3_pid[0], signal.SIGKILL)
+        print(bmc[0][0][0:-1])
+        #p_bmc.join()
+    else:
+        while (len(ic3) == 0) :
+            pass
+        print(ic3[0][0][0:-1])
+
+
+
