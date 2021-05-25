93c10d2c5fadbd79cfed6a6bdddaf7f3867b78f1
hu <1048448520@qq.com>
2021-05-25 16:25:01 +0800

    bug fixed

--- run_batch.py
+++ run_batch.py
@@ -25,13 +25,13 @@ def handle_rar_rec(folderpath):
         else:
             handle_rar_rec(newDir)                #如果不是文件，递归这个文件夹的路径
 
-def handle_tar(tgzpath):
+def handle_rar(tgzpath):
     tmp_package = os.path.splitext(tgzpath)[0]
     if os.path.exists(tmp_package):
         shutil.rmtree(tmp_package)
     os.mkdir(tmp_package)
-    os.system('echo unrar e ' + tgzpath + tmp_package + " >tmp.txt 2>&1")
-    os.system('tar -xzvf ' + tgzpath  + tmp_package) 
+    os.system('echo unrar e ' + tgzpath + tmp_package)
+    os.system('unrar e ' + tgzpath  + tmp_package + " >/dev/null 2>&1") 
     handle_rar_rec(tmp_package)
     shutil.rmtree(tmp_package)
 
@@ -42,8 +42,8 @@ def eachFile(filepath):
     for s in pathDir:
         newDir=os.path.join(filepath,s)     #将文件命加入到当前文件路径后面
         if os.path.isfile(newDir) :         #如果是文件
-            if os.path.splitext(newDir)[1]==".tar":  #判断是否是tar
-                handle_tar(newDir)                     #读文件（或者做其他的操作）
+            if os.path.splitext(newDir)[1]==".rar":  #判断是否是tar
+                handle_rar(newDir)                     #读文件（或者做其他的操作）
                 pass
         else:
             eachFile(newDir)                #如果不是文件，递归这个文件夹的路径
