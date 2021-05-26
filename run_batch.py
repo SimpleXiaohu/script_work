07522d3a282fb845def262d41bade3728d6171eb
hu <1048448520@qq.com>
2021-05-26 09:55:41 +0800

    add .rar arguement

--- run_batch.py
+++ run_batch.py
@@ -1,4 +1,4 @@
-import os
+import os,sys
 import subprocess
 import shutil
 
@@ -9,8 +9,9 @@ def run_ostrich(newDir):
     global cmd 
     ret = subprocess.run(f"{cmd} {newDir}",shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,encoding="utf-8")
     print(f"{newDir}:\n {ret.stdout}")
-    with open("res.txt", "a", encoding="utf8") as f:
+    with open(res, "a", encoding="utf8") as f:
         f.write(f"{newDir}:\n {ret.stdout}\n")
+        f.flush()
 
 
 def handle_rar_rec(folderpath):
@@ -30,8 +31,8 @@ def handle_rar(tgzpath):
     if os.path.exists(tmp_package):
         shutil.rmtree(tmp_package)
     os.mkdir(tmp_package)
-    os.system('echo unrar e ' + tgzpath + tmp_package)
-    os.system('unrar e ' + tgzpath  + tmp_package + " >/dev/null 2>&1") 
+    os.system(f'echo unrar e {tgzpath} {tmp_package}')
+    os.system(f'unrar e {tgzpath} {tmp_package} > /dev/null 2>&1') 
     handle_rar_rec(tmp_package)
     shutil.rmtree(tmp_package)
 
@@ -50,9 +51,9 @@ def eachFile(filepath):
 
 fileTailNum = 0
 cmd = "python3 script.py"
-
+res = f"{sys.argv[1][2:-4]}_res.txt"
 # clear the existing res file
-with open("res.txt", "r") as f:
+with open(res, "w") as f:
     f.close()
 
 eachFile('.')
